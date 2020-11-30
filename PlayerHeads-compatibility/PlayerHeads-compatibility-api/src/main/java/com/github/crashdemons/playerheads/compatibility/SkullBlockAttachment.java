6ed580bb9dd18dd3ae3ff52bc5a4da1136504042
crashdemons <crashenator@gmail.com>
2020-11-30 03:04:27 -0600

    add block information and setblock method to Compatibility-API reqs

---
+++ PlayerHeads-compatibility/PlayerHeads-compatibility-api/src/main/java/com/github/crashdemons/playerheads/compatibility/SkullBlockAttachment.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.crashdemons.playerheads.compatibility;
+
+/**
+ * Type of head blocks supported
+ * @author crashdemons (crashenator at gmail.com)
+ * @since 5.2.14-SNAPSHOT
+ */
+public enum SkullBlockAttachment {
+    FLOOR,
+    WALL,
+}
